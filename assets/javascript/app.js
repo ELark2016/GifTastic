a9248d02fbc02c11cf5e4f4136e61e61f6f5ddb9
Elizabeth Lark <elizabethlark@Elizabeths-MBP.fios-router.home>
2018-03-12 22:39:41 -0400

    able to retrive animated gifs for first search tearm and generate buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,66 @@
 
-// $("#submit-button").click(function() {
-//     event.preventDefault();
-// Build string for query to include desired parameters
-var QueryURL = "http://api.giphy.com/v1/gifs/search";
-QueryURL += '?' + $.param({
-   'limit': 10,
-   'api_key': "dJBEVywow51er4KCAp9ITnpl31KiCUAy",
-   'q': "tweety bird",
-});
-// ajax call that will retrieve results from Giphy.com per the parameters in the query url
-$.ajax({
-  url: QueryURL,
-  method: 'GET',
-})
-// returns the rating and images to the page
-.then(function(response) {
+var gifList = [];
 
-    for (var i = 0; i <= response.data.length; i ++){
-    var rating = response.data[i].rating;
-    var imageUrl = response.data[i].images.original.url; 
-    var pTag = $("<p>").text("Rating: " + rating);
-    var image = $("<img>");
-    image.attr("src", imageUrl);
-    image.attr("alt", "Image Placeholder");
-    $(".giphy").append(pTag);
-    $(".giphy").append(image);
-    console.log(response);
-    console.log(response.data.url);
-    }
-    
+// $(document).on("click", "#submit-button", displayMovieInfo);
+
+$("#submit-button").click(function(event){ 
+    event.preventDefault();
+
+    var search = $("#search-term").val().trim();
+    gifList.push(search);
+
+    var QueryURL = "http://api.giphy.com/v1/gifs/search";
+    QueryURL += '?' + $.param({
+    'limit': 10,
+    'api_key': "dJBEVywow51er4KCAp9ITnpl31KiCUAy",
+    'q': search,
+    });
+
+    console.log(QueryURL);
+    $.ajax({
+        url: QueryURL,
+        method: 'GET',
     })
-
-// })
+    .then(function(response) {
+        newButton();
+        for (var i = 0; i <= response.data.length; i ++){
+            var rating = response.data[i].rating;
+            var imageUrl = response.data[i].images.original.url; 
+            var pTag = $("<p>").text("Rating: " + rating);
+            pTag.attr("id", "img-rating");
+            $(".giphy").append(pTag);
+            var image = $("<img>");
+            image.attr("src", imageUrl);
+            image.attr("alt", "Image Placeholder");
+            $(".giphy").append(image);
+           
+            console.log(response);
+            console.log(imageUrl);
+        }
+    })
+})
 
 function newButton(){
-    var button = $("<button>Button</button>");
-    $("#img-button").append(button);
+    $("#img-button").empty();
+    for (var i = 0; i < gifList.length; i++) {
+        var search = $("#search-term").val().trim();
+        var button = $("<button>");
+        $("#img-button").append(button);
+        $("button").addClass("search-button");
+        button.attr("data-name", gifList[i]);
+        button.text(gifList[i]);
+        $("#img-button").append(button);    
+    }  
+    
+    
+}
+function clearButton(){
+    // $('#search-button').removeAttr('value');
+    $('#search-term').removeAttr('value');
 }
 
 
-
-
-
-
-
-// Add an on click function that will add image to page and create a button for the image
-    // add a for loop that will retrieve images up to the specified number
+    
+    // update so that a new search will present new images and keep prior buttons; maybe empty/clear search 
     // add an ajax call that will retrieve the still images
     // add a function that will "pause" the gif on click
