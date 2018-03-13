33d1222372addc2141409543e8008d33dbc816a9
Elizabeth Lark <elizabethlark@Elizabeths-MBP.fios-router.home>
2018-03-13 00:03:21 -0400

    updated code so that gifs will change when I add a new word in the search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,16 @@
 
 var gifList = [];
 
-// $(document).on("click", "#submit-button", displayMovieInfo);
 
 $("#submit-button").click(function(event){ 
     event.preventDefault();
+    gifDisplay();
+    newButton();
+    $(".giphy").empty();
+    
 
+})
+    function gifDisplay () {
     var search = $("#search-term").val().trim();
     gifList.push(search);
 
@@ -22,23 +27,42 @@ $("#submit-button").click(function(event){
         method: 'GET',
     })
     .then(function(response) {
-        newButton();
+        // newButton();
         for (var i = 0; i <= response.data.length; i ++){
             var rating = response.data[i].rating;
             var imageUrl = response.data[i].images.original.url; 
+            var stillImageUrl = response.data[i].images.original.still;
             var pTag = $("<p>").text("Rating: " + rating);
             pTag.attr("id", "img-rating");
             $(".giphy").append(pTag);
             var image = $("<img>");
+            image.addClass("gifImage");
             image.attr("src", imageUrl);
             image.attr("alt", "Image Placeholder");
-            $(".giphy").append(image);
-           
-            console.log(response);
-            console.log(imageUrl);
+            image.attr("data-animate", imageUrl);
+            image.attr("data-state", "animate");
+            image.attr("data-still", stillImageUrl);
+            image.attr("data-state", "still");
+            $(".giphy").append(image);   
         }
+            console.log(response);
+            console.log(image);
     })
-})
+}
+$(".gifImage").on("click", function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "animate") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
 
 function newButton(){
     $("#img-button").empty();
@@ -50,17 +74,13 @@ function newButton(){
         button.attr("data-name", gifList[i]);
         button.text(gifList[i]);
         $("#img-button").append(button);    
-    }  
-    
-    
-}
-function clearButton(){
-    // $('#search-button').removeAttr('value');
-    $('#search-term').removeAttr('value');
+    }   
 }
 
 
+
+
     
-    // update so that a new search will present new images and keep prior buttons; maybe empty/clear search 
+    // update so that a new search will present new images 
     // add an ajax call that will retrieve the still images
     // add a function that will "pause" the gif on click
